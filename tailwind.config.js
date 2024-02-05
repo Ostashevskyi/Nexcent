3283757e22f777fd3395815b23f288ec668fe12d
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2024-02-05 17:10:38 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,46 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        loading: "url(/loading.svg)",
+      },
+      colors: {
+        black: "var(--black)",
+        dark_grey: "var(--dark_grey)",
+        grey: "var(--grey)",
+        light_grey: "var(--light_grey)",
+        grey_blue: "var(--grey_blue)",
+        primary: "var(--primary)",
+        silver: "var(--silver)",
+        white: "var(--white)",
+        primary: "var(--primary)",
+        secondary: "var(--secondary)",
+        info: "var(--info)",
+        shade_1: "var(--shade_1)",
+        shade_2: "var(--shade_2)",
+        shade_3: "var(--shade_3)",
+        shade_4: "var(--shade_4)",
+        shade_5: "var(--shade_5)",
+        tint_1: "var(--tint_1)",
+        tint_2: "var(--tint_2)",
+        tint_3: "var(--tint_3)",
+        tint_4: "var(--tint_4)",
+        tint_5: "var(--tint_5)",
+        warning: "var(--warning)",
+        error: "var(--error)",
+        success: "var(--success)",
+      },
+      fontSize: {
+        64: "64px",
+        36: "36px",
+        28: "28px",
+        20: "20px",
+      },
+      lineHeight: {
+        76: "76px",
+        44: "44px",
       },
     },
   },
+
   plugins: [],
 };
