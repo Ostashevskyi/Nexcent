72c43e40f72fd8c080465e9382615974e1090023
Yuriy Ostashevskyi <80325154+Ostashevskyi@users.noreply.github.com>
2024-02-06 12:15:45 +0200

    created clients section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero";
+import OurClients from "@/components/OurClients";
 
 export default function Home() {
   return (
     <div className="max-container">
       <Hero />
+      <OurClients />
     </div>
   );
 }
